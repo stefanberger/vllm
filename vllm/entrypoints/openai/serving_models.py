ea74a35181d7e0b5cd1afc44f5b72fe1907f7d61
Stefan Berger <stefanb@linux.ibm.com>
2025-08-05 14:27:18 +0000

    Return reason in error message as to why LoRA could not be loaded
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- vllm/entrypoints/openai/serving_models.py
+++ vllm/entrypoints/openai/serving_models.py
@@ -241,6 +241,7 @@ class OpenAIServingModels:
             base_model_name = self.model_config.model
             unique_id = self.lora_id_counter.inc(1)
             found_adapter = False
+            reason = ""
 
             # Try to resolve using available resolvers
             for resolver in self.lora_resolvers:
@@ -263,13 +264,14 @@ class OpenAIServingModels:
                             "Failed to load LoRA '%s' resolved by %s: %s. "
                             "Trying next resolver.", lora_name,
                             resolver.__class__.__name__, e)
+                        reason = str(e)
                         continue
 
             if found_adapter:
                 # An adapter was found, but all attempts to load it failed.
                 return create_error_response(
                     message=(f"LoRA adapter '{lora_name}' was found "
-                             "but could not be loaded."),
+                             f"but could not be loaded: {reason}"),
                     err_type="BadRequestError",
                     status_code=HTTPStatus.BAD_REQUEST)
             else:
